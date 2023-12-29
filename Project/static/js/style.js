ca4299ab07da5938c5ed46e2a26a9f7f28a13043
quantran.23012003@gmail.com <quantran.23012003@gmail.com>
2023-12-29 15:47:21 +0700

    add mysql

--- Project/static/js/style.js
+++ Project/static/js/style.js
@@ -1,19 +1,38 @@
-//function toggleDarkMode() {
-//        var body = document.body;
-//        body.classList.toggle('dark-mode');
-//    }
-//
-//    function openProductModal(name, image, info) {
-//        var modalTitle = document.getElementById('exampleModalLabel');
-//        var modalImage = document.getElementById('modalImage');
-//        var modalPrice = document.getElementById('modalPrice');
-//        var modalDescription = document.getElementById('modalDescription');
-//
-//        modalTitle.textContent = name;
-//        modalImage.src = image;
-//        modalPrice.textContent = price;
-//        modalDescription.textContent = description;
-//
-//        var modal = new bootstrap.Modal(document.getElementById('productModal'));
-//        modal.show();
-//    }
+document.addEventListener('DOMContentLoaded', function () {
+        var body = document.body;
+        var darkModeToggle = document.querySelector('.dark-mode-toggle');
+
+        function toggleDarkMode() {
+            body.classList.toggle('dark-mode');
+            var isDarkMode = body.classList.contains('dark-mode');
+            localStorage.setItem('darkMode', isDarkMode);
+        }
+
+        darkModeToggle.addEventListener('click', toggleDarkMode);
+        var isDarkMode = localStorage.getItem('darkMode') === 'true';
+        if (isDarkMode) {
+            body.classList.add('dark-mode');
+        }
+        window.addEventListener('pageshow', function (event) {
+            var isDarkMode = localStorage.getItem('darkMode') === 'true';
+            if (isDarkMode) {
+                body.classList.add('dark-mode');
+            } else {
+                body.classList.remove('dark-mode');
+            }
+        });
+    });
+function handleViewDetailClick(name, image, info) {
+    // Hiển thị overlay
+    var detailOverlay = document.getElementById('detailOverlay');
+    detailOverlay.style.display = 'block';
+
+    // Cập nhật nội dung chi tiết
+    document.getElementById('detailName').textContent = name;
+    document.getElementById('detailImage').src = image;
+    document.getElementById('detailInfo').textContent = info;
+}
+function closeDetailOverlay() {
+    var detailOverlay = document.getElementById('detailOverlay');
+    detailOverlay.style.display = 'none';
+}
